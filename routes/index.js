3ba465a723cdfd70a37bd2b487ab2bdbdac51fef
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-31 14:27:39 -0500

    updates to routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+//const categoryRoutes = require('./category-routes');
+//const productRoutes = require('./product-routes');
+//const tagRoutes = require('./tag-routes');
 
 router.use('/api', apiRoutes);
+//do I need to define routes here?
+//router.use("/categories", require('./categories'));
+//router.use("/products", require('./products'));
+//router.use("/tags", tagsRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
